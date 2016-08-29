7c93abfc952008bf1d67fe98cf9a2190918c2a81
Ankit Chaudhary <achaudhary@stumbleupon.com>
2016-08-29 13:24:17 -0700

    keeping much smaller value for max_id_range to avoid any overflow bugs

--- sequencer/constants.py
+++ sequencer/constants.py
@@ -1,5 +1,5 @@
 id_generation_strategies = ['last-used', 'first-available']
-default_ids_range = 1024
+default_ids_range = 30
 default_aws_region = 'us-east-1'
 zk_sequencer_root = '/sequencer'
 autoscaling_grp_tag = 'aws:autoscaling:groupName'
